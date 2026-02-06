29a96ebe855091f026f6734d6cdcd64721c38871
networkpenetrationtester <x.nn77296.x@gmail.com>
2026-02-06 01:58:35 -0600

    Upgrades people, upgrades!

--- index.ts
+++ index.ts
@@ -66,14 +66,15 @@ class Logger {
 }
 
 type $config = {
-    "OverrideCachePath"?: string,
     "Logger": {
         "Requests": boolean,
         "Verbose": boolean,
         "LogFile": boolean,
     },
     "HttpServer": {
-        "StaticFiles"?: string[]
+        "OverrideCachePath"?: string,
+        "DirBrowser"?: boolean,
+        "StaticFiles"?: string[],
         "LocalHostName": string,
         "LocalPort": number,
         "Protocol": 'http'/*  | 'https' | 'ftp', */,
@@ -87,11 +88,11 @@ type $config = {
     }
 }
 
-const logger = new Logger(true, false);
 const DIR = import.meta.dirname;
 const HTTP_CACHE_PATH = path.join(DIR, 'www');
 const LOG_PATH = path.join(DIR, 'logs');
 const CONFIG_PATH = path.join(DIR, 'config.json');
+const logger = new Logger(true, false); // circular dependency headass
 const config: $config = (() => {
     try {
         return JSON.parse(fs.readFileSync(CONFIG_PATH, 'utf8'));
@@ -99,6 +100,7 @@ const config: $config = (() => {
         console.error(e);
         logger.Log(`* ERROR: ${CONFIG_PATH} failed to load! Proceeding with defaults...`);
         let config = {
+            "BasePath": null,
             "OverrideCachePath": HTTP_CACHE_PATH,
             "Logger": {
                 "Requests": true,
@@ -106,7 +108,8 @@ const config: $config = (() => {
                 "LogFile": false
             },
             "HttpServer": {
-                "StaticFiles": null,
+                "DirBrowser": false,
+                "StaticFiles": [],
                 "LocalHostName": "localhost",
                 "LocalPort": 8080,
                 "Protocol": "http",
@@ -123,7 +126,9 @@ const config: $config = (() => {
     }
 })();
 
-const OVERRIDE_CACHE_PATH = config.OverrideCachePath ?? HTTP_CACHE_PATH;
+logger.usefile = config.Logger.LogFile; // oh well its rough but it works :3
+
+const OVERRIDE_CACHE_PATH = config.HttpServer.OverrideCachePath ?? HTTP_CACHE_PATH;
 const REMOTE_ENDPOINT = `${config.Bouncer.Protocol}://${config.Bouncer.RemoteHostname}:${config.Bouncer.RemotePort}`;
 const HOSTED_ENDPOINT = `${config.HttpServer.Protocol}://${config.HttpServer.LocalHostName}:${config.HttpServer.LocalPort}`;
 
@@ -141,7 +146,8 @@ if (config.Logger.Verbose) {
     logger.Log(`* Program Started @ ${logger.CustomDate()}.`);
     logger.Log(`Remote Endpoint:\t${REMOTE_ENDPOINT}`);
     logger.Log(`Hosted Endpoint: \t${HOSTED_ENDPOINT}`);
-    logger.Log(`Cache Path: \t\t${config.OverrideCachePath}`);
+    logger.Log(`Cache Path: \t\t${OVERRIDE_CACHE_PATH}`);
+    logger.Log(`Dir Browser:\t\t${config.HttpServer.DirBrowser ? 'enabled' : 'disabled'}`);
     logger.Log(`Storing Cache:\t\t${config.HttpServer.StoreCache}`);
     logger.Log(`Serving Cache:\t\t${config.HttpServer.ServeCache}`);
     logger.Log('');
@@ -163,20 +169,39 @@ const RequestLogger: express.RequestHandler = (req, res, next) => {
 
 app.use(RequestLogger);
 
-if (config.HttpServer.StaticFiles) {
+if (config.HttpServer.StaticFiles) { // Wow I did it, I made it universal...
     for (let file of config.HttpServer.StaticFiles) {
         logger.Log(`* Exclusively serving from disk: ${file}`);
         app.get(file, (req, res) => {
             let filepath = path.join(OVERRIDE_CACHE_PATH, file)
             if (fs.existsSync(filepath)) {
+                logger.LogTime(`* Serving Static: ${filepath}`);
                 res.sendFile(filepath);
+                return;
             } else {
                 res.send(404);
+                return;
             }
         });
     }
     logger.Log('');
 }
+
+function dirBrowse(real_path: string, web_path: string, checked: boolean = false): string { // generate barebones html file browser
+    if (checked || fs.existsSync(real_path) && fs.statSync(real_path).isDirectory()) {
+        let children = ['..', ...fs.readdirSync(real_path)];
+        let links = children.map((child) => {
+            return `<div><a href="${path.join('/', web_path, child)}">${child}</a></div>`;
+        });
+        return ['<html>', '<body>', ...links, '</body>', '</html>'].join('\n');
+    }
+    return 'Not a Directory...';
+}
+
+app.get('/', (req, res) => {
+    config.HttpServer.DirBrowser ? res.send(dirBrowse(OVERRIDE_CACHE_PATH, '/')) : res.sendStatus(404);
+    return;
+});
 
 app.get('/*resource', async (req, res) => {
     try {
@@ -184,12 +209,17 @@ app.get('/*resource', async (req, res) => {
         let [request, rand] = parts;
 
         if (request.match(/([\\:?*"<>|]|\.{2})+/)) {
-            res.sendStatus(500);
+            res.sendStatus(418); // No hacking for you!
             return;
         }
 
         let absolute_path = path.join(OVERRIDE_CACHE_PATH, request);
         let exists = fs.existsSync(absolute_path);
+
+        if (exists && fs.statSync(absolute_path).isDirectory()) {
+            config.HttpServer.DirBrowser ? res.send(dirBrowse(absolute_path, request, true)) : res.sendStatus(404);
+            return;
+        }
 
         if (!exists || !config.HttpServer.ServeCache) { // if the cached file doesn't exist or we don't wanna serve cache (updating content), download resource.
             let url = REMOTE_ENDPOINT + request + (rand ? '?rand=' + rand : '');
@@ -204,10 +234,10 @@ app.get('/*resource', async (req, res) => {
                         fs.writeFileSync(absolute_path, buffer);
                     }
                     res.send(buffer);
-                })
-                .catch((e) => {
-                    console.error(e);
-                    res.sendStatus(500);
+                    return;
+                }).catch((e) => {
+                    res.sendStatus(e?.status || 500);
+                    return;
                 });
         }
 
@@ -216,9 +246,11 @@ app.get('/*resource', async (req, res) => {
                 logger.LogTime(`* Serving Cached: ${absolute_path}`);
             }
             res.sendFile(absolute_path);
+            return;
         }
     } catch (e) {
         console.error(e);
+        res.send(500);
     }
 });
 
